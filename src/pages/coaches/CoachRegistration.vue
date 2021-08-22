b3800b1aa38a73eedb0f402622a816e7594a315e
ArturM <amrozinski@gmail.com>
2021-08-22 11:38:24 +0100

    added coach registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -15,9 +15,10 @@ export default {
       },
     methods: {
         saveData(data) {
-            this.$store.dispatch('coaches/registerCoach', data)
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches')
         }
-    }
+    },
 };
 </script>
 
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,7 @@
     <section>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button to="/register" link>Register as a Coach</base-button>
+        <base-button v-if="!isCoach" to="/register" link>Register as a Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -56,7 +56,10 @@ export default {
     },
     hasCoaches() {
       return this.$store.getters['coaches/hasCoaches'];
-    }
+    },
+     isCoach() {
+        return this.$store.getters['coaches/isCoach']
+      }
   },
   methods: {
       setFilters(updatedFilters) {
